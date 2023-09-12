7023531dba1e0b977f1babf82150b0d112425ad8
JoshuaMeza <jmeza@cubicode.mx>
2023-09-11 19:27:57 -0600

    feat: Added bowling in OOP

--- inclass/src/3_TDD/objectOriented/BowlingGame.spec.ts
+++ inclass/src/3_TDD/objectOriented/BowlingGame.spec.ts
@@ -8,4 +8,47 @@ describe('Test Bowling Game Functionality', () => {
     game = new BowlingGame()
   });
 
+  it("should make a roll", () => {
+    expect(game.roll(0)).toBeUndefined();
+  });
+
+  it("should play an all zeros game", () => {
+    repeatThrow(20, 0, game);
+
+    expect(game.getFinalScore()).toBe(0);
+  });
+
+  it("should play an all ones game", () => {
+    repeatThrow(20, 1, game);
+
+    expect(game.getFinalScore()).toBe(20);
+  });
+
+  it("should play one spare", () => {
+    repeatThrow(2, 5, game);
+    repeatThrow(1, 3, game);
+    repeatThrow(17, 0, game);
+
+    expect(game.getFinalScore()).toBe(16);
+  });
+
+  it("should play one strike", () => {
+    repeatThrow(1, 10, game);
+    repeatThrow(2, 2, game);
+    repeatThrow(16, 0, game);
+
+    expect(game.getFinalScore()).toBe(18);
+  });
+
+  it("should play a perfect game", () => {
+    repeatThrow(12, 10, game);
+
+    expect(game.getFinalScore()).toBe(300);
+  });
 });
+
+function repeatThrow(rolls: number, pins: number, game: BowlingGame) {
+  for (let index = 0; index < rolls; index++) {
+    game.roll(pins);
+  }
+}
--- inclass/src/3_TDD/objectOriented/BowlingGame.ts
+++ inclass/src/3_TDD/objectOriented/BowlingGame.ts
@@ -1,6 +1,124 @@
+abstract class ScoreCalculator {
+  private currentIndex: number;
+  private pinsHistory: number[];
+
+  constructor (currentIndex: number, pinsHistory: number[]) {
+    this.currentIndex = currentIndex;
+    this.pinsHistory = pinsHistory;
+  }
+
+  public getCurrentIndex(): number {
+    return this.currentIndex;
+  }
+
+  public getPinsHistory(): number[] {
+    return this.pinsHistory;
+  }
+
+  public abstract getNextStartingIndex(): number;
+  public abstract calculate(): number;
+}
+
+class NormalCalculator extends ScoreCalculator {
+  public getNextStartingIndex(): number {
+    return this.getCurrentIndex() + 2;
+  }
+
+  public calculate(): number {
+    const INDEX = this.getCurrentIndex();
+    const PINS = this.getPinsHistory();
+
+    return PINS[INDEX] + PINS[INDEX+1];
+  }
+}
+
+class SpareCalculator extends ScoreCalculator {
+  public getNextStartingIndex(): number {
+    return this.getCurrentIndex() + 2;
+  }
+
+  public calculate(): number {
+    const INDEX = this.getCurrentIndex();
+    const PINS = this.getPinsHistory();
+
+    return PINS[INDEX] + PINS[INDEX+1] + PINS[INDEX+2];
+  }
+}
+
+class StrikeCalculator extends ScoreCalculator {
+  public getNextStartingIndex(): number {
+    return this.getCurrentIndex() + 1;
+  }
+
+  public calculate(): number {
+    const INDEX = this.getCurrentIndex();
+    const PINS = this.getPinsHistory();
+
+    return PINS[INDEX] + PINS[INDEX+1] + PINS[INDEX+2];
+  }
+}
+
+class ScoreCalculatorFactory {
+  public static getScoreCalculator(currentIndex: number, pinsHistory: number[]): ScoreCalculator {
+    const MAX_PINS = 10;
+
+    if (pinsHistory[currentIndex] == MAX_PINS) {
+      return new StrikeCalculator(currentIndex, pinsHistory);
+    } else if (pinsHistory[currentIndex] + pinsHistory[currentIndex+1] == MAX_PINS) {
+      return new SpareCalculator(currentIndex, pinsHistory);
+    } else {
+      return new NormalCalculator(currentIndex, pinsHistory);
+    }
+  }
+}
+
+class ScoreManager {
+  private pinsHistory: number[];
+
+  constructor () {
+    this.pinsHistory = [];
+  }
+
+  private getPinsHistory(): number[] {
+    return this.pinsHistory;
+  }
+
+  public addRollToHistory(pins: number): void {
+    this.getPinsHistory().push(pins);
+  }
+
+  public calculateFinalScore(): number {
+    const HISTORY = this.getPinsHistory();
+    const FRAMES = 10;
+    let total = 0;
+    let currentIndex = 0;
+
+    for (let i = 0; i < FRAMES; i++) {
+      const SCORE_CALCULATOR = ScoreCalculatorFactory.getScoreCalculator(currentIndex, HISTORY);
+      total += SCORE_CALCULATOR.calculate();
+      currentIndex = SCORE_CALCULATOR.getNextStartingIndex();
+    }
+
+    return total;
+  }
+}
 
 export class BowlingGame {
+  private score: ScoreManager;
 
-  constructor() { }
+  constructor() {
+    this.score = new ScoreManager();
+  }
 
+  private getScore(): ScoreManager {
+    return this.score;
+  }
+
+  public getFinalScore(): number {
+    return this.getScore().calculateFinalScore();
+  }
+
+  public roll(pins: number): void {
+    this.getScore().addRollToHistory(pins);
+  }
 }
