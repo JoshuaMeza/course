cb6fb6e9e6cd2d8d0faef093c8aca0dfafb8f0d3
JonathanGGB <jonathangogbz@gmail.com>
2023-11-05 14:31:12 -0600

    ME-3 Servicio de mejores ataques

--- projects/solidExcercise/src/pokemonInfo/handlers/PokemonInfoHandler.ts
+++ projects/solidExcercise/src/pokemonInfo/handlers/PokemonInfoHandler.ts
@@ -1,14 +1,28 @@
 import { PokemonInfoService } from '../services/PokemonInfoService'
 import { mapToPokemonDto } from '../utils/mapToPokemonDataDto'
+import any = jasmine.any;
 
 export class PokemonInfoHandler extends PokemonInfoService {
   public async getInfo(id: number) {
     const getAllResponse = await this.getPokemonRepository().getAll(id)
-    const baseInformation = this.getBaseInformation(getAllResponse)
+    const filterMovesInformation = this.filterMoves(getAllResponse)
+    const baseInformation = this.getBaseInformation(filterMovesInformation)
 
     return {
       baseInformation
     }
+  }
+
+  private filterMoves(pokemon: any) {
+    const moves = pokemon.moves
+
+    moves.sort((a: any, b: any) => b.version_group_details[0].level_learned_at - a.version_group_details[0].level_learned_at)
+
+    const topMoves = moves.slice(0, 4)
+
+    pokemon.moves = topMoves
+
+    return pokemon
   }
 
   private getBaseInformation(pokemon: any) {
