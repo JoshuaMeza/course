fbcf9411bfe067f4bde040302faf85792ef9e707
José Fernando Chan Dzib <56355924+Jfeerchan23@users.noreply.github.com>
2023-11-05 14:01:16 -0600

    correccion pokemón countries requester

--- projects/solidExcercise/src/pokemonInfo/requesters/PokemonCountriesRequester.ts
+++ projects/solidExcercise/src/pokemonInfo/requesters/PokemonCountriesRequester.ts
@@ -1,11 +1,12 @@
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
+/* eslint-disable @typescript-eslint/naming-convention */
 import { runQuery } from '../../db'
 import { PokemonCountriesRepository } from '../repositories/PokemonCountriesRepository'
 
 export class PokemonCountriesRequester implements PokemonCountriesRepository {
 
   public async getCountries(id: number) {
-    // eslint-disable-next-line @typescript-eslint/naming-convention
+
     const families_id = await this.getFamilies_id(id)
     const query = `SELECT z.country_code
     FROM family_zone fz 
@@ -13,11 +14,7 @@ export class PokemonCountriesRequester implements PokemonCountriesRepository {
     WHERE fz.family_id IN (${families_id}) 
     GROUP BY z.country_code
     ORDER BY MAX(fz.probability) DESC LIMIT 3`
-    /*  */
-
     const results = await runQuery(query).then(response => response.results)
-
-
     const countryCodes = results.map(result => result.country_code)
 
     return countryCodes
@@ -25,10 +22,9 @@ export class PokemonCountriesRequester implements PokemonCountriesRepository {
 
 
   public async getFamilies_id(id: number) {
+
     const query = `SELECT family_id FROM pokedb.family_pokemon WHERE pokemon_id = ${id}`
     const results = await runQuery(query).then(response => response.results)
-
-    // Utilizar map para extraer los valores de family_id
     const familyIds = results.map(result => result.family_id)
 
     return familyIds
